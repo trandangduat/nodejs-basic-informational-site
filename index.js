483405795a9f302b98b66d2b142ab40d6d384ce3
Tran Dang Duat <23020023@vnu.edu.vn>
2024-05-11 09:21:29 +0700

    npm & express installed

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-const http = require("http");
-const url  = require("url");
-const fs   = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-http.createServer(function (req, res) {
-    const url_module = url.parse(req.url, true);
-    let filename = "." + url_module.pathname + ".html";
-    fs.stat(filename, function (err, stats) {
-        if (err) {
-            res.writeHead(404, {"Content-Type": "text/html"});
-            fs.readFile("./404.html", function (err, data) {
-                res.write(data);
-                return res.end();
-            });
-        } 
-        else {
-            fs.readFile(filename, function (err, data) {
-                if (err) {
-                    res.writeHead(500, {"Content-Type": "text/html"});
-                    res.write("Server Error");
-                    return res.end();
-                }
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(data);
-                return res.end();
-            });
-        }
-    });
-    
-}).listen(8080);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
